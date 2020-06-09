114d029563e0926c133ae12a9fc42bfcd0125b16
rafael_polonio <rafael.polonio.ext@neon.com.br>
2020-06-09 10:09:32 -0300

    creating detail and point screen

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Image, Text } from 'react-native';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 import Constants from 'expo-constants';
+import api from '../../services/api';
 
+interface Params {
+  point_id: number;
+};
+
+interface Data {
+  point: {
+    image: string,
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  };
+  items: {
+    title: string;
+  }[];
+}
 const Detail = () => {
+  const [data, setData] = useState<Data>({} as Data) 
   const navigation = useNavigation();
+  const route = useRoute();
+  
+  const routeParams = route.params as Params;
 
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(res => {
+      setData(res.data);
+    })
+  })
   function handleNavigateBack() {
     navigation.goBack();
+  };
+
+  if (!data.point) {
+    return null;
   };
 
   return (
@@ -19,13 +50,13 @@ const Detail = () => {
           <Icon name='arrow-left' size={23} color='#34cb79'/>
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: 'https://blog.guiabolso.com.br/wp-content/uploads/2018/02/mercado-1-1-700x525.jpg'}}></Image>
-        <Text style={styles.pointName}>Mercadao</Text>
-        <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+        <Image style={styles.pointImage} source={{ uri: data.point.image }}></Image>
+        <Text style={styles.pointName}>{data.point.name}</Text>
+        <Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
 
         <View style={styles.address}>
           <Text style={styles.addressTitle}>Endereço</Text>
-          <Text style={styles.addressContent}>Guaratinguetá, SP</Text>
+          <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
         </View>
       </View>
       <View style={styles.footer}>
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { Image, View, StyleSheet, Text, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
 import api from '../../services/api';
+import * as Location from 'expo-location'
 
 interface Item {
   id: number;
@@ -13,11 +14,50 @@ interface Item {
   image_url: string;
 };
 
+interface Point {
+  id: number;
+  image: string,
+  name: string,
+  latitude: number;
+  longitude: number;
+}
+
 const Points = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [selectedItems, setSelectedItems ] = useState<number[]>([]);
-
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+  const [points, setPoints] = useState<Point[]>([]);
   const navigation = useNavigation();
+
+  useEffect(() => {
+    async function loadPosition() {
+      const { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Ooops', 'Precisamos da sua permissao para obter a localização')
+        return ;
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+      setInitialPosition([
+        latitude,
+        longitude,
+      ])
+
+    }
+  });
+
+  useEffect(() => {
+    api.get('points', {
+      params: {
+        city: 'Guaratinguetá',
+        uf: 'SC',
+        items: [1, 2]
+      }
+    }).then((res) => {
+      setPoints(res.data)
+    })
+  });
 
   useEffect(() => {
     api.get('items').then((res) => {
@@ -29,8 +69,8 @@ const Points = () => {
     navigation.goBack();
   };
 
-  function handleNavigateDetail() {
-    navigation.navigate('Detail');
+  function handleNavigateDetail(id: number) {
+    navigation.navigate('Detail', { point_id: id });
   };
 
   const handleSelectItem = (id: number) => {
@@ -55,23 +95,33 @@ const Points = () => {
         <Text style={styles.description}>Encontre no mapa um ponto de coleta</Text>
 
         <View style={styles.mapContainer}>
-          <MapView 
+          { initialPosition[0] !== 0 && (
+            <MapView 
             style={styles.map} 
             initialRegion={{
-              latitude: -21.9963952, 
-              longitude: -47.9009033,
+              latitude: initialPosition[0], 
+              longitude: initialPosition[1],
               latitudeDelta: 0.014,
               longitudeDelta: 0.014,
             }}
           >
-            <Marker 
-              onPress={handleNavigateDetail}
-              coordinate={{ 
-                latitude: -21.9963952, 
-                longitude: -47.9009033,
+            {points.map(point => (
+              <Marker 
+                key={String(point.id)}
+                onPress={() => handleNavigateDetail(point.id)}
+                coordinate={{ 
+                latitude: point.latitude, 
+                longitude: point.longitude,
               }}
-            />
+              >
+                <View style={styles.mapMarkerContainer}>
+                  <Image style={styles.mapMarkerImage} source={{ uri: point.image }} />
+                  <Text style={styles.mapMarkerTitle}>{ point.name }</Text>
+                </View>
+              </Marker>
+            ))}
           </MapView>
+          )}
         </View>
       </View>
       <View style={styles.itemsContainer}>
